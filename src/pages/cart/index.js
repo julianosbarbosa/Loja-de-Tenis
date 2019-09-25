3a9662bf40bfc6f66abf6ab3c355541bedb3df8f
Juliano <julianotrabalho2016@gmail.com>
2019-09-25 18:54:54 -0300

    UPDATE - Layout Cart page

--- src/pages/cart/index.js
+++ src/pages/cart/index.js
@@ -1,7 +1,66 @@
 import React from "react";
 
-// import { Container } from './styles';
+import { Container, ProductTable, Total } from "./styles";
+import {
+  MdRemoveCircleOutline,
+  MdAddCircleOutline,
+  MdDelete
+} from "react-icons/md";
 
-const Cart = () => <h1>Cart</h1>;
+const Cart = () => (
+  <Container>
+    <ProductTable>
+      <thead>
+        <tr>
+          <th />
+          <th>PRODUTO</th>
+          <th>QTD</th>
+          <th>SUBTOTAL</th>
+          <th />
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>
+            <img
+              src="https://static.netshoes.com.br/produtos/tenis-nike-shox-nz-masculino/12/D12-9660-112/D12-9660-112_zoom1.jpg"
+              alt="Tenis Nike"
+            />
+          </td>
+          <td>
+            <strong>Tenis Nike-Shox</strong>
+            <span>R$499,00</span>
+          </td>
+          <td>
+            <div>
+              <button type="button">
+                <MdRemoveCircleOutline size={20} color="#7159c1" />
+              </button>
+              <input type="number" readOnly value={3} />
+              <button type="button">
+                <MdAddCircleOutline size={20} color="#7159c1" />
+              </button>
+            </div>
+          </td>
+          <td>
+            <strong>R$1496,00</strong>
+          </td>
+          <td>
+            <button type="button">
+              <MdDelete size={20} color="#7159c1" />
+            </button>
+          </td>
+        </tr>
+      </tbody>
+    </ProductTable>
+    <footer>
+      <button type="button">Finalizar Pedido</button>
+      <Total>
+        <span>TOTAL</span>
+        <strong>R$1520,00</strong>
+      </Total>
+    </footer>
+  </Container>
+);
 
 export default Cart;
