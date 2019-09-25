55182d93e140dc2de96db0061047d5711a7ebffc
Juliano <julianotrabalho2016@gmail.com>
2019-09-25 17:39:24 -0300

    UPDATE - layout home

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,6 +1,25 @@
 import React from "react";
+import { ProductList } from "./styles";
+import { MdAddShoppingCart } from "react-icons/md";
 
-// import { Container } from './styles';
-
-const Home = () => <h1>Home</h1>;
+const Home = () => (
+  <ProductList>
+    <li>
+      <img
+        src={
+          "https://static.netshoes.com.br/produtos/tenis-nike-shox-nz-masculino/12/D12-9660-112/D12-9660-112_zoom1.jpg"
+        }
+        alt="Tenis nike"
+      />
+      <strong>Tenis Nike-Shox</strong>
+      <span>R$499,00</span>
+      <button type="button">
+        <div>
+          <MdAddShoppingCart size={16} color="#fff" /> 3
+        </div>
+        <span>ADICIONAR AO CARRINHO</span>
+      </button>
+    </li>
+  </ProductList>
+);
 export default Home;
