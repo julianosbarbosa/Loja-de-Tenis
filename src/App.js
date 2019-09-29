aff264ba50cba85489fdfea11a24953ef105bf73
Juliano <julianotrabalho2016@gmail.com>
2019-09-29 14:21:04 -0300

    UPDATE - configurate reactotron in redux

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
+import "./config/ReactotronConfig";
 import Routes from "./routes";
 import GlobalStyle from "./styles/global";
 import Header from "./components/Header";
